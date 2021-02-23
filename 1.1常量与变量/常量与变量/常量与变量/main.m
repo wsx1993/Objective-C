6af4fb1cd8a1adc24ab85bd4041e84e8f696921a
baozi <2535586605@qq.com>
2021-02-23 12:47:12 +0800

    no message

---
+++ 1.1常量与变量/常量与变量/常量与变量/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  常量与变量
+//
+//  Created by 包子 on 2021/2/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
